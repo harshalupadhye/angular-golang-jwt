a3300ce9fb7513da6c8670963382f24f75a56ce5
harshal <harshal@experience.com>
2022-04-02 15:15:17 +0530

    information in comment for withcredentials: true

--- web/src/app/services/jwt.service.ts
+++ web/src/app/services/jwt.service.ts
@@ -14,9 +14,10 @@ export class JwtService {
   }
   checkUser = (user: string) =>{
     return this.http.post(`${this.endpoint}/signin`, user, { withCredentials: true })
-    //{ withCredentials: true } most imp thing otherwise token wont be saved at browser check config at server side
+    //{ withCredentials: true } most imp thing otherwise token wont be saved at browser check config at server side 
+    //a boolean value that indicates whether or not cross-site Access-Control requests should be made using credentials such as cookies, authorization headers or TLS client certificates
   }
   goHome = () =>{
-    return this.http.get(`${this.endpoint}/home`, { withCredentials: true })
+    return this.http.get(`${this.endpoint}/home`, { withCredentials: true})
   }
 }
