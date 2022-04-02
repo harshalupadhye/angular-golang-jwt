665e25087c3f9924aebc063c17818e4a3cfb6e3e
harshal <harshal@experience.com>
2022-04-02 15:14:49 +0530

    delete cookie on logout

--- web/src/app/component/home/home.component.ts
+++ web/src/app/component/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { JwtService } from 'src/app/services/jwt.service'; 
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -8,8 +8,15 @@ import { JwtService } from 'src/app/services/jwt.service';
 })
 export class HomeComponent implements OnInit {
   username : any = null   
-  constructor(public jwtService : JwtService) { }
-
+  constructor(public jwtService : JwtService, public router: Router) { }
+  logout = () =>{
+    let matchedCookie: string | any = document.cookie
+      .match('(^|;)\\s*' + 'token' + '\\s*=\\s*([^;]+)')
+      ?.pop()
+      console.log(matchedCookie)
+      document.cookie = `token=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;`;
+      this.router.navigateByUrl('signin')
+  }
   ngOnInit(): void {
     this.jwtService.goHome().subscribe((user: any)=>{
       console.log('user',user)
